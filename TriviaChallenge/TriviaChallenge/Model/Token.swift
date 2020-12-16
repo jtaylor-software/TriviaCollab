875693c270ac8ec8f7d1847ac158a644df6e2140
Jeremy Taylor <jtaylor@jctaylor.org>
2020-12-16 15:14:54 -0600

    Added model for session token

---
+++ TriviaChallenge/TriviaChallenge/Model/Token.swift
@@ -0,0 +1,12 @@
+//
+//  Token.swift
+//  TriviaChallenge
+//
+//  Created by Jeremy Taylor on 12/16/20.
+//
+
+import Foundation
+
+struct Token: Codable {
+    let token: String
+} // end of struct
