19f29fe764c258c5512ae2e3be7b02a0fcdd8c3a
Jeremy Taylor <jtaylor@jctaylor.org>
2020-12-16 13:49:44 -0600

    Completed Trivia Question Model

---
+++ TriviaChallenge/TriviaChallenge/Model/Question.swift
@@ -0,0 +1,25 @@
+//
+//  Question.swift
+//  TriviaChallenge
+//
+//  Created by Jeremy Taylor on 12/16/20.
+//
+
+import Foundation
+
+struct Question: Codable {
+    let question: String
+    let correctAnswer: String
+    let incorrectAnswers: [String]
+    
+    enum CodingKeys: String, CodingKey {
+        case question
+        case correctAnswer = "correct_answer"
+        case incorrectAnswers = "incorrect_answers"
+        
+    }
+} //end of struct
+
+struct TopLevelJSON: Codable {
+    let results: [Question]
+} //end of struct
