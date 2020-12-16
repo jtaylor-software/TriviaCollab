587cfb8d5aa5f290480c92bb8644f5920c2db275
Jeremy Taylor <jtaylor@jctaylor.org>
2020-12-16 13:22:48 -0600

    Added initial project

---
+++ TriviaChallenge/TriviaChallenge/View Controllers/SelectionsViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SelectionsViewController.swift
+//  TriviaChallenge
+//
+//  Created by Jeremy Taylor on 12/16/20.
+//
+
+import UIKit
+
+class SelectionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+} //end of class
