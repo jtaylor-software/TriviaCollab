1a8f41868a126f1452fd39a631431415ce27fedc
Jeremy Taylor <jtaylor@jctaylor.org>
2020-12-17 15:19:49 -0600

    Revert "Jeremy work"

--- TriviaChallenge/TriviaChallenge/Model/Question.swift
+++ TriviaChallenge/TriviaChallenge/Model/Question.swift
@@ -8,12 +8,12 @@
 import Foundation
 
 struct Question: Codable {
-    let questionText: String
+    let question: String
     let correctAnswer: String
     let incorrectAnswers: [String]
     
     enum CodingKeys: String, CodingKey {
-        case questionText
+        case question
         case correctAnswer = "correct_answer"
         case incorrectAnswers = "incorrect_answers"
         
--- TriviaChallenge/TriviaChallenge/View Controllers/SelectionsViewController.swift
+++ TriviaChallenge/TriviaChallenge/View Controllers/SelectionsViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 class SelectionsViewController: UIViewController {
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
